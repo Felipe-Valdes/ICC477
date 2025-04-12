d68d06568e6b7cbd91cf6ff3c0e535f39bd755b2
seba2424 <s.calfunao01@ufromail.cl>
2025-04-12 19:53:06 -0400

    Cambios de estructura de respuesta a modo diccionario para chatbot

--- Proyecto/chatbot_project/chatbot.py
+++ Proyecto/chatbot_project/chatbot.py
@@ -18,33 +18,21 @@ nlp = spacy.load("es_core_news_sm")
 # Se han añadido comentarios para explicar cada parte del código.
 
 def chatbot_response(text):
-    doc = nlp(text)
-
-    # Respuestas básicas de saludo
-    if "hola" in text.lower():
-        return "¡Hola! ¿Cómo estás?"
-    elif "adiós" in text.lower():
-        return "¡Adiós! ¡Que tengas un buen día!"
-    elif "cómo estás" in text.lower():
-        return "¡Estoy bien, gracias por preguntar! ¿Y tú?"
-    
-    # Consultas para el valor del dólar y UF
-    elif "dólar" in text.lower():
-        return obtener_dolar()
-    elif "uf" in text.lower():
-        return obtener_uf()
-    
-    # Consultas para el clima (sin parámetro de ciudad, siempre será Santiago)
-    elif "clima" in text.lower():
-        return obtener_clima()  
-    
-    # Consultas para las noticias (siempre serán de business en inglés y traducidas al español)
-    elif "noticias" in text.lower():
-        return obtener_noticias()  
-    
-    # Respuesta por defecto si no se entiende el comando
-    else:
-        return "Lo siento, no te entendí. ¿Puedes repetirlo?"
+    text= text.lower()  # Convertir el texto a minúsculas para facilitar la comparación
+    responses={
+        "hola":"hola, ¿cómo estás?",
+        "adios":"adiós, ¡que tengas un buen día!",
+        "como estas":"estoy bien, gracias por preguntar.",
+        "dólar": obtener_dolar,
+        "uf": obtener_uf,
+        "noticias": obtener_noticias,
+        "clima": obtener_clima,
+    }
+    for key,value in responses.items():
+        if key in text:
+            return value() if callable(value) else value
+        
+    return "Lo siento, no entiendo tu pregunta. ¿Puedes reformularla?"
 
 # Interacción con el usuario
 # Se inicia un bucle que permite al usuario interactuar con el chatbot
@@ -52,10 +40,8 @@ def chatbot_response(text):
 print("¡Hola! Soy tu chatbot. Escribe 'adiós' para salir.")
 while True:
     user_input = input("Tú: ")
-    
+    response = chatbot_response(user_input)
+    print(f"Chatbot: {response}")
     if "adiós" in user_input.lower():
         print("Chatbot: ¡Adiós! ¡Que tengas un buen día!")
         break
-
-    response = chatbot_response(user_input)
-    print("Chatbot:", response)
